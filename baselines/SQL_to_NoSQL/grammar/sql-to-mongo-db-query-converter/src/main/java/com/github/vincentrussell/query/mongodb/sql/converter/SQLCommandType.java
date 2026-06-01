1105295f98c4d8b6d4c93bf38a2acde7383dce42
Jiawei-Lu-0712 <1749010569ljw@gmail.com>
2026-06-01 21:05:39 +0800

    delete outdated baselines

--- baselines/SQL_to_NoSQL/grammar/sql-to-mongo-db-query-converter/src/main/java/com/github/vincentrussell/query/mongodb/sql/converter/SQLCommandType.java
+++
@@ -1,8 +0,0 @@
-package com.github.vincentrussell.query.mongodb.sql.converter;
-
-/**
- * A enum which describes the type of SQL operation this is.
- */
-public enum SQLCommandType {
-    DELETE, SELECT, UPDATE
-}
